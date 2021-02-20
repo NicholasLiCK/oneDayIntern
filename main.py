63dbea89cbf48a161910bf6268482cf71f5ff9e7
Sunny <shensunny594@gmail.com>
2021-02-20 15:56:50 +0800

    feat: searchStr

--- main.py
+++ main.py
@@ -4,20 +4,28 @@ with open('ptt_dump_20210218_1141.json', 'r') as f:
     file = json.load(f)
 
 
-# def searchStr(input_str, all_post):
-    
-    
-#     return
+def searchStr(input_str):
+    to_output = []
+    for article in file['articles']:
+        if input_str in article['article_title']:
+            to_output.append(article)
+        elif input_str in article['content']:
+            to_output.append(article)
+        elif input_str in article['messages']:
+            to_output.append(article)
+            
+    return to_output
 
 
-to_output = []
-for article in file['articles']:
-    if 'Re' in article['article_title']:
-        to_output.append(article)
-    elif 'Re' in article['content']:
-        to_output.append(article)
-    elif 'Re' in article['']
-
-print(to_output)
+if __name__ == '__main__':
     
+    print('Articles')
+    print('Alisasa')
+    print('PUIPUI')
+    
+    request = input("Enter one of above:")
+    
+    ans = searchStr(request)
+    
+    print(ans)
     
